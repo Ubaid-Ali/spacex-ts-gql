526e50c688931d772a3169229580f8a82fc8d96b
Ubaid-Ali <syedubaidali2020@gmail.com>
2020-12-23 17:37:02 +0530

    update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,7 +2,8 @@ const CACHE_NAME = 'SpaceX';
 const urlsToCache = [
     '/index.html',
     '/static/js/bundle.js',
-    '/0.chunk.js'
+    '/static/js/0.chunk.js',
+    '/'
 ];
 
 
@@ -38,37 +39,20 @@ self.addEventListener('fetch', (event) => {
 });
 
 
-// Activate the service worker
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
+// Update a service worker
+self.addEventListener('activate', function (event) {
 
-                return fetch(event.request)
-                    .then((response) => {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+    let cacheAllowlist = [...urlsToCache];
 
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheAllowlist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
                     }
-                    );
-            })
+                })
+            );
+        })
     );
 });
